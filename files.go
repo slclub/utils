40125c025864ec74f115cb70d06cd150578f88fe
slclub@163.com <slclub@163.com>
2020-06-15 07:33:49 +0800

    function IsFileExist panic fix

--- files.go
+++ files.go
@@ -14,7 +14,7 @@ func IsFileExist(path string) (bool, error) {
 		return false, nil
 	}
 	//我这里判断了如果是0也算不存在
-	if fileInfo.Size() == 0 {
+	if fileInfo != nil && fileInfo.Size() == 0 {
 		return false, nil
 	}
 
